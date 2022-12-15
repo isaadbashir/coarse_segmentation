2de443afca6ad1c236fa7bb19d747c3c64d361c7
Saad Bashir <17871817+rajasaad@users.noreply.github.com>
2022-12-15 15:19:53 +0000

    coarse masks generation code

---
+++ config.py
@@ -0,0 +1,8 @@
+# global path  variables
+PIXELWISE_PATCHES_PATH = '/mnt/sda2/coarse_segmentation/data/patches/pixel_wise/'
+COARSEWISE_PATCHES_PATH = '/mnt/sda2/coarse_segmentation/data/patches/coarse_wise/'
+PATCHES_MAGNIFICATION_LEVEL = 20
+
+## pre-processing
+# coarse masks
+COARSE_FILTER_SIZE = 32
