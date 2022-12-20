7ad0101791446e4b2f7a75472517773190178809
Saad Bashir <17871817+rajasaad@users.noreply.github.com>
2022-12-20 11:19:41 +0000

    Add: Data reader for reading the coarse masks
    - data reader added
    - fixed coarse masks data type from float to uint8 for coarse masks
    - added keys for use
    - added utils calss
    - refactored tnbc relabelling function to utils

--- config.py
+++ config.py
@@ -3,45 +3,20 @@ PIXELWISE_PATCHES_PATH = '/mnt/sda2/coarse_segmentation/data/patches/pixel_wise/
 COARSEWISE_PATCHES_PATH = '/mnt/sda2/coarse_segmentation/data/patches/coarse_wise/'
 PATCHES_MAGNIFICATION_LEVEL = '20x' # must of be string as 10x, 20x, 40x
 
+# number of classes
+TNBC_NUMBER_OF_CLASSES = 5
+
 ## pre-processing
 # coarse masks
-# filter size i.e., the window max will be taken and assigned
-COARSE_FILTER_SIZE = 16
+# filter size i.e., the window max will be taken and assigned [8,16,32]
+COARSE_FILTER_SIZE = 32
 
-
-# common functions used in the project
-def group_mask_tnbc_classes(mask):
-    """
-    Groups the minor mask classes into major ones according to the info given in the paper
-    :param mask:
-    """
-    # tumor cluster
-    mask[mask == 19] = 1
-    mask[mask == 20] = 1
-
-    # inflammatory
-    mask[mask == 10] = 3
-    mask[mask == 11] = 3
-
-    # others
-    mask[mask == 5] = 5
-    mask[mask == 6] = 5
-    mask[mask == 7] = 5
-    mask[mask == 8] = 5
-    mask[mask == 9] = 5
-    mask[mask == 10] = 5
-    mask[mask == 11] = 5
-    mask[mask == 12] = 5
-    mask[mask == 13] = 5
-    mask[mask == 14] = 5
-    mask[mask == 15] = 5
-    mask[mask == 16] = 5
-    mask[mask == 17] = 5
-    mask[mask == 18] = 5
-    mask[mask == 19] = 5
-    mask[mask == 20] = 5
-    mask[mask == 21] = 5
-
-    mask[mask == 5] = 0
-
-    return mask
+# dict keys for saving patches
+# image
+KEY_IMAGE = 'img'
+# original mask
+KEY_MASK = 'omask'
+# coarse mask smaller
+KEY_COARSE_MASK = 'cmask'
+# coarse mask of size as orignial mask
+KEY_COARSE_MASK_RESIZE = 'mask'
