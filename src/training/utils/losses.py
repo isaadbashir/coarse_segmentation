d518cf953f4dd725729fa2240132427494a0da01
Saad Bashir <17871817+rajasaad@users.noreply.github.com>
2023-01-05 02:51:38 +0000

    Fix: Fix SCE loss

--- src/training/utils/losses.py
+++ src/training/utils/losses.py
@@ -39,16 +39,15 @@ class SCELoss(torch.nn.Module):
         self.alpha = alpha
         self.beta = beta
         self.num_classes = num_classes
-        self.cross_entropy = torch.nn.CrossEntropyLoss()
 
     def forward(self, pred, labels):
         # CCE
-        ce = self.cross_entropy(pred, labels)
+        ce = F.cross_entropy(pred, labels)
 
         # RCE
         pred = F.softmax(pred, dim=1)
         pred = torch.clamp(pred, min=1e-7, max=1.0)
-        label_one_hot = F.one_hot(labels, self.num_classes).float().to(self.device)
+        label_one_hot = F.one_hot(labels, self.num_classes).float().to(self.device).permute(0,3,1,2)
         label_one_hot = torch.clamp(label_one_hot, min=1e-4, max=1.0)
         rce = (-1*torch.sum(pred * torch.log(label_one_hot), dim=1))
 
